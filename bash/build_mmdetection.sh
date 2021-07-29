578c1eb8be7c379f781a69a3970b78c2af950298
liuyunhaozz <75206746+liuyunhaozz@users.noreply.github.com>
2021-07-29 22:52:35 +0800

    Update build_mmdetection.sh
    fix bugs

--- bash/build_mmdetection.sh
+++ bash/build_mmdetection.sh
@@ -1,4 +1,4 @@
-#!usr/bin/bash
+#!/usr/bin/bash
 pip install torch==1.8.0+cu101 torchvision==0.9.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 pip install openmim
 mim install mmdet
